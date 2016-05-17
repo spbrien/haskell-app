760b3c5d09a09cbc2cb712f2781a7fc2a74ddb45
Steven Brien <spbrien@gmail.com>
2016-05-17 09:37:35 -0400

    working through composable html templates

--- src/Main.hs
+++ src/Main.hs
@@ -2,26 +2,31 @@
 
 module Main where
 
+import Control.Monad (forM_)
 import Web.Scotty
 import qualified Web.Scotty as S
 import qualified Text.Blaze.Html5 as H
 -- import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.Text
+import Network.Wai.Middleware.RequestLogger
 
-
-page :: [Char] -> H.Html
+page :: H.Html -> H.Html
 page x = H.html $ do
     H.head $ do
         H.title "Page"
-    H.body $ do
-        H.div $ do
-            H.p $ do
-                H.toHtml $ x
+    H.body $ x
+
+blockContent :: H.Html -> H.Html
+blockContent x = H.div x
+
+paragraph :: [Char] -> H.Html
+paragraph x = H.p $ do
+    H.toHtml $ x
 
 main :: IO()
 main = do
   scotty 3000 $ do
+    middleware logStdoutDev
     get "/:name" $ do
-        name <- param "name"
         S.html . renderHtml $ do
-            page name
+            page $ blockContent $ forM_ ["one", "two"] (paragraph)
